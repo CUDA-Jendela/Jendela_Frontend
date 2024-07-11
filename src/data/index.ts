8c48e67bf96cce2ba743c0c22a1590d8ee44101e
mikeleo03 <leonmichael463@gmail.com>
2024-07-12 01:04:29 +0700

    [Feat] Add section on home

---
+++ src/data/index.ts
@@ -0,0 +1,39 @@
+import feature1 from "@/assets/images/feature1.jpg"
+import feature2 from "@/assets/images/feature2.jpg"
+import feature3 from "@/assets/images/feature3.jpg"
+import feature4 from "@/assets/images/feature4.jpg"
+import feature5 from "@/assets/images/feature5.jpg"
+import feature6 from "@/assets/images/feature6.jpg"
+
+export const featureCardData = [
+    {
+        imageSrc: feature1,
+        title: 'Course List',
+        description: "Access specialized courses tailored to ex-prisoners' skills, demonstrating their capability beyond past records.",
+    },
+    {
+        imageSrc: feature2,
+        title: 'Hiring Access',
+        description: 'Enable companies to hire trained and vetted ex-prisoners, fostering inclusivity in the workplace.',
+    },
+    {
+        imageSrc: feature3,
+        title: 'NGO Participation',
+        description: 'NGOs can offer impactful courses that equip ex-prisoners with essential skills for workplace readiness.',
+    },
+    {
+        imageSrc: feature4,
+        title: 'AI Recommendations',
+        description: 'Utilize AI to provide personalized training tracks based on individual talents and aptitudes.',
+    },
+    {
+        imageSrc: feature5,
+        title: 'Tracked Assessment',
+        description: "Monitor and ensure ex-prisoners' performance during a 1-year post-hire period, guaranteeing commitment and growth.",
+    },
+    {
+        imageSrc: feature6,
+        title: 'Awareness Increase',
+        description: "Highlight ex-prisoners' contributions, showcasing their skills and potential for a brighter future.",
+    },
+];
