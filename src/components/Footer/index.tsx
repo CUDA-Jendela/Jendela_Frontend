90d3e69855f91d7134153a3350420d9858356829
mikeleo03 <leonmichael463@gmail.com>
2024-07-11 23:51:51 +0700

    [Feat] Footer implementation

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import logoWhite from "@/assets/logo_white.png";
+import { Link } from 'react-router-dom';
+
 const Navbar: React.FC = () => {
     return (
-        <div className="flex items-center justify-between bg-gray-800 p-5 px-8 z-50 text-white">
-            INI FOOTER
+        <div className="flex flex-row items-center justify-between bg-primary bg-main2 p-5 pb-8 px-8 z-50 text-white">
+            {/* Logo */}
+            <Link to="/" className="text-white text-lg font-bold transition-transform duration-300 transform hover:scale-110">
+                <img src={logoWhite} className="w-16"></img>
+            </Link>
+
+            <div className='pt-3 text-white font-semibold'>
+                © 2024 CUDA GarudaHacks 5.0
+            </div>
         </div>
     )
 };
