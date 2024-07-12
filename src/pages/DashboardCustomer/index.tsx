90a4b473b88fe2d1f81420076fef2e72f87c61dc
mikeleo03 <leonmichael463@gmail.com>
2024-07-12 23:52:09 +0700

    [Feat] Redirection over dashboard

--- src/pages/DashboardCustomer/index.tsx
+++ src/pages/DashboardCustomer/index.tsx
@@ -1,15 +1,33 @@
 import useAuth from "@/contexts/AuthContext";
-import React from "react";
-import { Courses } from "@/data";
+import React, { useEffect, useState } from "react";
+// import { Courses } from "@/data";
 import { CourseCard } from "@/components";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import dashboardHeader from "@/assets/svg/dashboard_header.svg";
+import Cookies from "js-cookie";
+import { CourseCardProps, CoursesResponse } from "@/types";
+import { CourseApi } from "@/api";
 
 const DashboardCustomer: React.FC = () => {
-    const { username } = useAuth();
+    const { username, role } = useAuth();
     const navigate = useNavigate();
+    const [courses, setCourses] = useState<CourseCardProps[]>([]);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            try {
+                const jwtToken = Cookies.get("j-token");
+                const courseData: CoursesResponse = await CourseApi.getRecommendation(jwtToken as string);
+                setCourses(courseData.data);
+            } catch (error) {
+                console.error('Failed to fetch data:', error);
+            }
+        };
+
+        fetchCourses();
+    }, []);
 
     const navigateToCourse = () => {
         navigate('/course');
@@ -23,11 +41,11 @@ const DashboardCustomer: React.FC = () => {
                 <h1 className="text-6xl font-bold text-primary">Hello, {username}</h1>
                 <h1 className="text-4xl text-black">Welcome back! we're happy to see you again.</h1>
 
-                <h3 className="mt-8 text-4xl font-bold">Courses for you</h3>
+                { role == "customer" && <><h3 className="mt-8 text-4xl font-bold">Courses for you</h3>
                 <p className="mt-1 text-lg">We found some courses that match your interests!</p>
                 <Carousel className="w-full max-w-full p-2 mt-2">
                     <CarouselContent className="flex space-x-0">
-                        {Courses.map((course) => (
+                        {courses.map((course) => (
                             <CarouselItem key={course.id} className="flex-shrink-0 w-80 my-4 mx-3 md:basis-1/2 lg:basis-1/3">
                                 <CourseCard {...course} />
                             </CarouselItem>
@@ -36,6 +54,7 @@ const DashboardCustomer: React.FC = () => {
                     <CarouselPrevious />
                     <CarouselNext />
                 </Carousel>
+                </> }
                 
                 <div className="flex flex-row items-center mt-16">
                     <div className="flex flex-col">
