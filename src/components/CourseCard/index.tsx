5063f87aa1a1cee8c964a3734e97f3c275ad2a55
arleenchr <13521059@mahasiswa.itb.ac.id>
2024-07-12 11:32:08 +0700

    [fix] skill tags overflow

--- src/components/CourseCard/index.tsx
+++ src/components/CourseCard/index.tsx
@@ -14,11 +14,11 @@ const Card: React.FC<CourseCardProps> = ({ id, title, ngoName, location, startDa
         <Link to={`/courseDetail/${id}`}>
             <div
                 key={id}
-                className="border h-[250px] overflow-hidden border-2 rounded-lg flex cursor-pointer transform transition-transform hover:scale-105 hover:shadow-lg"
+                className="max-w-sm h-fit overflow-hidden shadow-lg rounded-2xl flex cursor-pointer transform transition-transform hover:scale-105 hover:shadow-lg"
             >
-                <div className="w-[100px] bg-green-yellow-gradient border rounded-lg">
+                <div className="w-[100px] bg-green-yellow-gradient">
                 </div>
-                <div className="w-full p-5">
+                <div className="max-w-[75%] p-5">
                     <h2 className="text-2xl font-bold">{title}</h2>
                     <p className="text-base pb-3 text-gray-500">{ngoName}</p>
                     <div className="flex items-center text-base">
@@ -33,9 +33,9 @@ const Card: React.FC<CourseCardProps> = ({ id, title, ngoName, location, startDa
                         <FaUsers className="mr-2 text-gray-500" />
                         Quota: {quota}
                     </div>
-                    <div className="mt-2">
+                    <div className="flex mt-2 overflow-hidden">
                         {skills.map(skill => (
-                            <span key={skill} className="bg-gray-200 px-2 py-1 rounded-full text-sm mr-2">
+                            <span key={skill} className="bg-gray-200 px-2 py-1 rounded-full text-sm mr-2 whitespace-nowrap">
                                 {skill}
                             </span>
                         ))}
