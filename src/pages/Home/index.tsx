fcbb05d321329cd424f034e7f10b4a9e19ee2c3e
arleenchr <13521059@mahasiswa.itb.ac.id>
2024-07-12 09:57:35 +0700

    [fix] home banner to svg

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,26 +6,20 @@ import { FAQItem, featureCardData } from "@/data";
 import logoWhite from "@/assets/logo_white.png";
 import { Card } from "@/components";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import banner from "@/assets/svg/home_banner.svg";
 
 const Home: React.FC = () => {
     return (
         <main className="flex flex-col bg-white w-full min-h-screen">
-            {/* Hero Section */}
-            <div className="bg-custom-gradient w-full bg-full bg-no-repeat h-[90vh]">
+            <img src={banner} className="mb-14 z-0" />
+            <div className="absolute left-0 flex flex-col z-10">
                 <div className="w-1/2 flex flex-col align-center pl-20 pt-20">
                     <img src={logoWhite} className="w-64 pb-4"></img>
                     <h3 className="text-white text-lg">Jendela empowers ex-prisoners by providing skill development through NGO training and connecting them with job opportunities, ensuring they can contribute positively to society.</h3>
                 </div>
                 <div className="pl-20 pt-10">
-                    <Button className="border rounded-3xl text-xl p-6 border-white text-white hover:bg-white hover:text-primary hover:font-bold">Get to know more</Button>
+                        <Button className="border rounded-3xl text-xl p-6 border-white text-white hover:bg-white hover:text-primary hover:font-bold">Get to know more</Button>
                 </div>
-            </div>
-
-            {/* Separator */}
-            <div className="h-[150px] overflow-hidden">
-                <svg viewBox="0 0 500 150" preserveAspectRatio="none" className="h-full w-full">
-                    <path d="M0.00,50.10 C150.00,150.33 271.29,-50.10 501.31,87.12 L500.00,-0.00 L0.00,-0.00 Z" className="stroke-none fill-current text-primary"></path>
-                </svg>
             </div>
 
             {/* Main content */}
