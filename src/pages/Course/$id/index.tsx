916d14f0456c21fdb90dba4ac96ab436c5f458a4
mikeleo03 <leonmichael463@gmail.com>
2024-07-13 00:14:50 +0700

    [Feat] Integration based on course ID

--- src/pages/Course/$id/index.tsx
+++ src/pages/Course/$id/index.tsx
@@ -2,32 +2,30 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { FaMapMarkerAlt, FaCalendarAlt, FaUsers } from "react-icons/fa";
 import { format } from "date-fns";
-import { CourseCardProps } from "@/types";
-import { Courses } from "@/data";
+import { CourseCardProps2, CourseResponse } from "@/types";
+// import { Courses } from "@/data";
 import { Button } from "@/components/ui/button";
+import Cookies from "js-cookie";
+import { CourseApi } from "@/api";
 
 const CourseDetail: React.FC = () => {
     const { id } = useParams<{ id: string }>();
-    // const [course, setCourse] = useState<CourseCardProps>();
-    const course = Courses.find((course) => course.id === id);
+    const [course, setCourse] = useState<CourseCardProps2>();
 
-    // useEffect(() => {
-    //     // Replace with actual API endpoint
-    //     const fetchCourse = async () => {
-    //         try {
-    //             const response = await fetch(`/api/courses/${id}`);
-    //             if (!response.ok) {
-    //                 throw new Error('Network response was not ok');
-    //             }
-    //             const data = await response.json();
-    //             setCourse(data);
-    //         } catch (error) {
-    //             console.error('Fetch error:', error);
-    //         }
-    //     };
+    useEffect(() => {
+        // Replace with actual API endpoint
+        const fetchCourse = async () => {
+            try {
+                const jwtToken = Cookies.get("j-token");
+                const courseData: CourseResponse = await CourseApi.getById(jwtToken as string, id as string);
+                setCourse(courseData.data);
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        };
 
-    //     fetchCourse();
-    // }, [id]);
+        fetchCourse();
+    }, [id]);
 
     if (!course) {
         return <div>Loading...</div>;
@@ -45,12 +43,12 @@ const CourseDetail: React.FC = () => {
                     <div className="h-50 w-[30%] bg-green-yellow-gradient rounded-lg"></div>
 
                     <div className="flex flex-col gap-2 py-5 h-full w-[60%] justify-center">
-                        <h2 className="text-3xl font-bold">{course.title}</h2>
+                        <h2 className="text-3xl font-bold">{course.name}</h2>
                         <p className="text-lg text-gray-500">{course.ngoName}</p>
 
                         <div className="flex items-center text-lg">
                             <FaMapMarkerAlt className="mr-2 text-gray-500" />
-                            {course.location}
+                            {course.ngoCity}
                         </div>
                         <div className="flex items-center text-lg">
                             <FaCalendarAlt className="mr-2 text-gray-500" />
@@ -69,7 +67,7 @@ const CourseDetail: React.FC = () => {
                 <h3 className="mt-6 text-2xl font-bold">Skills</h3>
                 <div className="mt-2">
                     {course.skills.map((skill: string) => (
-                        <span key={skill} className="bg-gray-200 px-2 py-1 rounded-full text-base mr-2">
+                        <span key={skill} className="bg-gray-200 px-3 py-2 rounded-full text-base mr-2">
                             {skill}
                         </span>
                     ))}
